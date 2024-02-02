f2575792acfc88d84cfe6430ceca72a8b1189f04
EricAngel1 <eric.angel9288@alumnos.udg.mx>
2024-02-02 10:56:23 -0600

    Segunda Actividad

---
+++ PrimerPrograma.cpp
@@ -0,0 +1,24 @@
+/*
+ Eric Eduardo Angel Angel
+ Centro universitario de los altos
+ verion 1.0
+
+ */
+//Agregamos librerias
+#include <iostream>
+
+//Definimos el std
+using namespace std;
+
+    //iniciamos el main
+    int main(){
+        //Declaramos variables
+        int edad = 18;
+
+        //Imprimimos texto
+        cout<<"mi nombre es Eric Eduardo Angel Angel"<<endl;
+        cout<<"Mi edad es "<<edad<<endl;
+
+        //Regresamos 0 en caso de ejecutarse exitosamente
+        return 0;
+    }
